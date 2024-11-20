2ae9fac5b4437072671ccf244ba49020303d8359
Daniel Coburn <138069778+Coburn05@users.noreply.github.com>
2024-11-19 19:27:41 -0500

    Update Heap.java

--- Heap.java
+++ Heap.java
@@ -4,7 +4,7 @@ import java.util.*;
  * A data structure that implements a maximum heap. The heap's internal storage
  * should be based on a standard Java array.
  */
-interface Heap<T extends Comparable<? super T>> {
+public interface Heap<T extends Comparable<? super T>> {
 	/**
 	 * Adds the specified item to the heap.
 	 * This operation must run in logarithmic time <em>O</em>O(log <em>n</em>), where <em>n</em>
